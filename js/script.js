22198e4f3ab09c144d309bffb3f12f60850f704e
stellakim1123 <glamstellakim@gmail.com>
2025-06-27 16:51:08 +0900

    25.06.27

--- js/script.js
+++ js/script.js
@@ -6,10 +6,18 @@ $('.menu>li').mouseover(function(){
 });
 });
 
-// function slide(){
-//     $('.slide').stop().animate({marginLeft:'-100%'}, 'slow', function(){
-//         $('.slide li:first').appendTo('.slide');
-//         $('.slide').css({marginLeft:0});
-//     })
-// }
-// setInterval(slide, 3000)
+function slide(){
+    $('.slide').stop().animate({marginLeft:'-100%'}, 'slow', function(){
+        $('.slide li:first').appendTo('.slide');
+        $('.slide').css({marginLeft:0});
+    })
+}
+setInterval(slide, 3000)
+
+$(function(){
+    $('.roombox').hover(function(){
+        $(this).children('.room_text').stop().animate({'opacity':'1', 'top':'40%'},'slow' );
+    }, function(){
+        $(this).children('.room_text').stop().animate({'opacity':'0.5', 'top':'80%'},'slow' );
+    });
+});
